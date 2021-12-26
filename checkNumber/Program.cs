33652f63b5f0e47313d7987bf597ba36aed1b6f5
Eldor Zandani <fox18966@gmail.com>
2021-12-26 18:04:46 +0200

    Add project files.

---
+++ checkNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please press a number");
+            string str_input = Console.ReadLine();
+            int a = int.Parse(str_input);
+            
+            if ((a >= 5) && (a <= 10))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
